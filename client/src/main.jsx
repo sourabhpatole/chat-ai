095b35d3cc206b0415fd59cfba44e704db4068c2
Sourabh <sourabhpatole@outlook.com>
2026-05-22 23:22:38 +0530

    feat: update React and ReactDOM versions, add routing with React Router, and implement layout components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./routes/homePage/HomePage.jsx";
+import DashboardPage from "./routes/dashboardpage/DashboardPage";
+import ChatPage from "./routes/chatPage/chatPage";
+import RootLayout from "./layouts/RootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/dashboardLayout";
+
+const router = createBrowserRouter([
+  {
+    Component: RootLayout,
+    children: [
+      {
+        path: "/",
+        Component: HomePage,
+      },
+      {
+        Component: DashboardLayout,
+        children: [
+          { path: "/dashboard", Component: DashboardPage },
+          { path: "/dashboard/chats/:id", Component: ChatPage },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
--- client/src/routes/chatPage/chatPage.jsx
+++ client/src/routes/chatPage/chatPage.jsx
@@ -1,6 +1,7 @@
 import "./chatPage.css";
 
 const ChatPage = () => {
+  
   return <div className="chatPage">ChatPage</div>;
 };
 
