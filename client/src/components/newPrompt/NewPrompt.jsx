9c7fd76f463fcd192316edf4ace88d33d8fa0cf2
Sourabh <sourabhpatole@outlook.com>
2026-05-24 02:37:00 +0530

    fix: correct image paths in NewPrompt component

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -14,12 +14,12 @@ const NewPrompt = () => {
       <div className="endChat" ref={endRef}></div>
       <form action="" className="newForm">
         <label htmlFor="file">
-          <img src="attachment.png" alt="" />
+          <img src="/attachment.png" alt="" />
         </label>
         <input id="file" type="file" multiple={false} hidden />
         <input type="text" placeholder="Ask anything.." />
         <button>
-          <img src="arrow.png" alt="" />
+          <img src="/arrow.png" alt="" />
         </button>
       </form>
     </>
